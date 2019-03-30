c0766d8770591fb82a85b341c7ed23038484038f
Akbar30bill <alishahverdi2010@yahoo.com>
2019-03-30 18:19:53 +0430

    1

--- 1.py
+++ 1.py
@@ -2,13 +2,13 @@ a = int(input())
 lst = list(map(int,input().split()))
 lst.insert(0,10**10)
 lst.append(10**10)
-print(lst)
-kkd = [0]*a
-sizer,sizel,rt,lt = [0],[0],[],[]
+# print(lst)
+sizer,sizel,rt,lt = [],[],[],[]
 for i in range(1,a+1):
     while True:
         if len(rt) == 0:
-            sizer.append(i)
+            rt.append(i)
+            sizer.append(i - 1)
             break
         else:
             if lst[i] < lst[rt[-1]]:
@@ -18,20 +18,25 @@ for i in range(1,a+1):
                 break
             else:
                 rt.pop()
-    print(rt)
-for i in range(a,1,-1):
+    # print(rt)
+for i in range(a,0,-1):
     while True:
         if len(lt) == 0:
-            sizel.append(i)
+            lt.append(i)
+            sizel.append(a - i)
             break
         else:
             if lst[i] < lst[lt[-1]]:
                 lt.append(i)
-                try:sizel.append(lt[-1]-lt[-2])
+                try:sizel.append(lt[-2]-lt[-1])
                 except:sizel.append(0)
                 break
             else:
                 lt.pop()
-    print(lt)
-for i in range(a):
-    print(max([sizel[i],sizer[i]]))
+    # print(lt)
+# print(sizel)
+# print(sizer)
+sizel.reverse()
+for i in range(0 , a):
+    print(max([sizel[i],sizer[i]]),end=" ")
+print()
