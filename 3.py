3f24daf973fa9819866fc5f6533e21b9d7694721
Akbar30bill <alishahverdi2010@yahoo.com>
2019-03-31 12:26:16 +0430

    3wrk

--- 3.py
+++ 3.py
@@ -1,6 +1,8 @@
 lll=int(input())
 mdn=list(map(int,input().split()))
-lst=[]
+lst=[0]*lll
+for i in range(lll):
+    lst[mdn[i]] += 1
 for i in range(lll):
     lst.append(mdn.count(mdn[i]))
 stk=[]
@@ -12,7 +14,7 @@ for i in range(lll-1 , -1 , -1):
             sstk.append(-1)
             break
         else:
-            if lst[i] < lst[stk[-1]]:
+            if lst[mdn[i]] < lst[mdn[stk[-1]]]:
                 stk.append(i)
                 sstk.append(mdn[stk[-2]])
                 break
